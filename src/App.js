fcf6cbcd1a56699c14fd125e2138af364607c600
Newville23 <newville21@gmail.com>
2017-10-14 16:37:26 -0500

    fix: All in order

--- src/App.js
+++ src/App.js
@@ -29,14 +29,12 @@ class BooksApp extends Component {
     let { books } = this.state
     return (
       <div className="app">
-      <Router history={hashHistory} onUpdate={logPageView}>
         <Route exact path="/" render={() => (
           <ListBooks books={books} moveBook={this.updateBooks}/>
         )}/>
         <Route path="/search" render={() => (
           <SearchBooks books={books} moveBook={this.updateBooks}/>
         )}/>
-        </Router>
       </div>
     )
   }
