58c0c256b2ca7e20c999597f955311535a238776
Newville23 <newville21@gmail.com>
2017-10-14 13:53:02 -0500

    refactor: Change BrowserRouter to hashRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter, BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
      <App />
-    </BrowserRouter>
+    </HashRouter>
 , document.getElementById('root'))
